de19beacd829aad410bc6ef536a5879d39949fe0
jace-ys <jaceys.tan@gmail.com>
2021-05-08 00:29:17 +0800

    Create website

---
+++ public/js/main.js
@@ -0,0 +1,26 @@
+(function($) {
+  "use strict";
+
+  var $WIN = $(window);
+  var doc = document.documentElement;
+  doc.setAttribute("data-useragent", navigator.userAgent);
+
+  var ssPreloader = function() {
+    $("html").addClass("ss-preload");
+
+    $WIN.on("load", function() {
+      $("#loader").fadeOut("slow", function() {
+        $("#preloader")
+          .delay(300)
+          .fadeOut("slow");
+      });
+
+      $("html").removeClass("ss-preload");
+      $("html").addClass("ss-loaded");
+    });
+  };
+
+  (function ssInit() {
+    ssPreloader();
+  })();
+})(jQuery);
